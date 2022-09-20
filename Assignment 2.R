c2744f4df23f46d69682b6b492119b14e7239b0f
Troy Adair <tra418@lehigh.edu>
2022-09-20 19:17:38 -0400

    Assignment 2 Graded

--- Assignment 2.R
+++ Assignment 2.R
@@ -4,6 +4,9 @@ library(dplyr)
 penn <-data %>% filter(state == "Pennsylvania")
 
 incr_cases <-diff(penn$cases)
+
+# TAA Failed to calculate row 1 as per different approach   -10
+
 incr_deaths<-diff(penn$deaths)
 
 sd(incr_cases)
